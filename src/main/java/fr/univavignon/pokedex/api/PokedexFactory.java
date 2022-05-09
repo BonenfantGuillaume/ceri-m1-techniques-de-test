d4e281959bf6214195cf705bd4e39c85759c73a1
ci-build <ci-build@klukas.net>
2022-05-09 09:17:42 +0000

    Updates

--- src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
+++
@@ -1,8 +0,0 @@
-package fr.univavignon.pokedex.api;
-
-public class PokedexFactory implements IPokedexFactory{
-    @Override
-    public Pokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
-        return new Pokedex(pokemonFactory, metadataProvider);
-    }
-}
